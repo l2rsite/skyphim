0895d16c78b6739d4bf70ef450f4e56d2e2da12f
l2rsite <40783788+l2rsite@users.noreply.github.com>
2020-05-12 16:21:11 +0700

    Update skyphim.php

--- skyphim.php
+++ skyphim.php
@@ -1,8 +1,54 @@
-<?php
+<html>
+<title>SkyPhim*Net</title>
+<style type="text/css">
+			html, body {
+				height: 100% !important;
+				width: 100% !important;
+				padding: 0 !important;
+				margin: 0 !important;
+			}
+		#SkyPhim{
+				height: 100% !important;
+				width: 100% !important; 
+				padding: 0 !important;
+				stretching: 'none';
+			}
+			
 
-$actual_link = "$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-$all_links = explode("/", $actual_link);
-$link = $all_links['2'];
-echo $link;
+</style>
+<body onselectstart="return false" oncontextmenu="return false">
+<div id="SkyPhim">Loading ...</div -->
+<script type="text/javascript" src="https://cdnjs.skyphim.net/jwplayer.js"></script>
+<script type="text/javascript">jwplayer.key="cLGMn8T20tGvW+0eXPhq4NNmLB57TrscPjd1IyJF84o=";</script>
+	<script type="text/javascript">
+		jwplayer("SkyPhim").setup({
+			playlist: [{
+				"sources":<?php echo $source;?>,
+			}],
+			
+				allowfullscreen: true,
+				width: '100%',
+				aspectratio: '16:9',
+				autostart: true,
+				mute: false,
+				cast: {},
+				logo: {
+        file: 'https://cdnjs.skyphim.net/skyphim.png'
+    			},
+				primary:'html5'
 
-?>
+		});
+
+</script>
+<script>
+jwplayer("SkyPhim").addButton("https://cdnjs.skyphim.net/FILE/forward.svg", "Next 30s", function () {
+            jwplayer("SkyPhim").seek(jwplayer("SkyPhim").getPosition() + 30);
+        }, "Next 30s");
+jwplayer("SkyPhim").addButton("https://cdnjs.skyphim.net/FILE/backward.svg", "Back 30s", function () {
+            jwplayer("SkyPhim").seek(jwplayer("SkyPhim").getPosition() - 30);
+        }, "Back 30s");
+
+</script>
+
+</body>
+</html>
